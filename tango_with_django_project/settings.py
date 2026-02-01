6f1e84df8bc7d30e7f4970b7190f6d36510f3847
Serena Tang <3179914t@student.gla.ac.uk>
2026-02-01 14:28:51 +0000

    Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length' : 6}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -129,3 +130,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT= MEDIA_DIR
 MEDIA_URL = '/media/'
+LOGIN_URL = 'rango:login'
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
